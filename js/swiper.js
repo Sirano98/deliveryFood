9abad5311d0653f7e272d25d871c82797215e9ad
Sirano98 <protsiuk1998@gmail.com>
2020-11-13 00:11:35 +0200

    from lesson

---
+++ js/swiper.js
@@ -0,0 +1,12 @@
+new Swiper(".swiper-container", {
+    slidesPerView: 1,
+    loop: true,
+    grabCursor: true,
+    autoplay: {
+        delay: 3000
+    },
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+    }
+})
